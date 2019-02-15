c4184c97d70affd4346799bf0ff02030f8ae34e3
Elf M. Sternberg <elf.sternberg@gmail.com>
2019-02-15 13:31:08 -0800

    FEAT: The Heavyweight Experiments, only now with Rust!

--- rust/07_heavyweights/README.md
+++ rust/07_heavyweights/README.md
@@ -1,63 +1,55 @@
-# Rigged Glushkov Regular Expressions in Rust
+# Rigged Generic Glushkov Regular Expressions in Rust: The Heavyweight Experiments
 
-This code is significantly different from the Haskell version (Haskell
-Experiment 07), in that I decided to "go for it" and merge the process
-of instantiation and rigging into a single structure. 
+This implementation is significantly different than previous ones,
+although it does build off that work, and it does proceed directly from
+the [Haskell implementation](../../haskell/08_Heavyweights/) and the
+[previous Rust experiment](../../rust/06_riggedglushkov).
 
-Prior to Rust Experiment 06, the Rust experiments had followed the
-Haskell versions' pattern of building the regular expression first using
-Kleene expressions, and then lifting them on-the-fly into more complex,
-"rigged" versions, before processing them with the given Kleene or
-Glushkov construction.
+I strongly recommend reading the [Haskell implementation
+README](../../haskell/08_Heavyweights/README.md) to get a sense of the
+changes to the algorithm.  They are fairly heavyweight and interesting,
+in that the definition of the Semiring has been further abstracted to
+handle position information, and as a consequence the input type of the
+operation has likewise been abstracted to handle arbitrary input types.
+The last was done to enable us to pass both the character being analyzed
+and the position in the stream, such that we could record information
+about the position under certain circumstances.
 
-Rust famously doesn't have memory management, but instead uses lifetimes
-and scopes to place much of what it does safely on the stack.  It takes
-some fiddling to make types, lifetimes, and scopes line up, so as far
-back as the first Rust experiment I had individual factory functions for
-the different Regex sub-types.  These take the place of the simple
-`data` types seen in the equivalent Haskell experiments.
+What makes the *Rust* version of this implementation noteworthy is the
+ease with which the inbound data type was changed to handle just about
+any kind of data.  It adds a bit of genericizing noise to the source
+file, some ceremony that makes me wonder how I could abstract or derive
+it automatically.
 
-With that in mind, there was no reason to have two different data
-structures; for this experiment, there is only the one `enum` types and
-its sub-types.
+On the other hand, since the `Recognizer` and `Parser` implementations
+concretize that their input type is `char` by usage, they work *completely
+unchanged* from the previous Rust experiment.  That's remarkable.
 
-In this experiment, as in the Haskell version, I build on their idea of
-recording already found "empty" and "final" versions of nodes, so the
-data structure is now a record of (empty, final, expression).  The
-`emp`, `alt`, `seq`, and `rep` expressions are pretty much as you'd
-expect; one thing you won't find in the base code is the implementation
-for `sym`.  `sym` must implemented independently for different semiring
-implementations.
+The implementation of the `Leftlong` Semiring, which reports the
+location and length of the first, longest substring match of a capture
+group (yes!) is fairly extensive and went through a number of thrashes
+before I recalled that I could match on a tuple, at which point the
+implementations of `add` and `mul` became straightforward.
 
-The `Sym` expression is a *trait* now; it says that users must provide
-an implementation with a single method, `is`, that takes a symbol and
-returns a semiring.
+Putting the entirety of the semiring in a single trait makes more sense,
+to me at least, than abstracting it further over `num_traits`, as I had
+it in earlier versions.  While using `num_traits` is *clever*, it also
+forces us to work with the `::Mul` and `::Add`, which do not take
+references, and for larger and more complex semirings, working with
+references made a lot of sense.
 
-I've had to abandon the use of `num_traits` and `std::ops`, as
-`std::ops::Mul` and `std::ops::Add` don't provide a framework for
-passing in references.  I've gone back to my initial instincts and
-provided a comprehensive `Semiring` trait which can take references for
-the `mul` and `add` operations.  This works very well, as now I can
-analyze and operate on immutable `HashSet<String>` collections without
-having to clone them to pass them to the cartesian product operation.
-That's a massive win in terms of memory and CPU savings.
+The implementation of `submatch`, a function that allows us to search
+for arbitrary substrings without having to root or ceiling the string is
+interesting; by using the `One()` value, I'm able to preserve the fact
+that the search hasn't failed while also enforcing the notion that we're
+skipping over things that match `any` but don't match the concrete
+sample, which is nifty.
 
-The construction processing the expression using Gluskov's progressive
-algorithm is the same as the unrigged version, only we cache the "empty"
-and "final" values when they're found and do not recalculate them.
+All in all, this is highly satisfying work, and it's a pleasure to see
+it working so well.
 
-Down in the tests, you'll find the Boolean version (`Recognizer`) as
-well as a string version (`Parser`).  Both versions show how to
-implement a semiring for doing data extraction, including how to define
-a specific Sym implementation for the `Sym` trait, and include a
-function to instantiate that implementation for your use case.
+## License
 
-The `Parser` version has a specific moment of complexity that can't be
-elided: in the `mul` implementation, the multiplication of two sets is
-the cartesian product of those sets: a new set containing all possible
-combinations of ordered tuples made up of a member of the first set with
-a member of the second set.  For our purposes, this is a still a string,
-so our implementation involves building those tuples and then generating
-a new string by concatenating the orderded pair.  This takes a bit of
-memory thrashing, but much less now that I've solved the `mul(&x, &y)`
-issue.
+As this is entirely my work, it is copyright (c) 2019, and licensed
+under the Mozilla Public License v. 2.0.  See the
+[LICENSE.md](../../LICENSE.md) in the root directory.
--- rust/07_heavyweights/src/lib.rs
+++ rust/07_heavyweights/src/lib.rs
@@ -4,11 +4,17 @@
 //! value describing if the expression matched the string (or not).
 //!
 
-use std::iter::IntoIterator;
-use std::iter::Iterator;
+// Generic type conventions:
+//   R: Our Ring Type
+//   D: Our inbound Data type
+//   I: An Iterator type
+
+#[allow(unused_imports)]
 use std::collections::HashSet;
+use std::iter::Iterator;
 use std::rc::Rc;
 
+#[allow(unused_macros)]
 macro_rules! matches {
     ($expression:expr, $($pattern:tt)+) => {
         match $expression {
@@ -25,21 +31,21 @@ pub trait Semiring {
     fn mul(&self, rhs: &Self) -> Self;
     fn add(&self, rhs: &Self) -> Self;
 }
-    
+
 /// The Sym trait represents what to do for a single character.  It has
 /// a single method, "is", that returns the semiring.  Implementers of
 /// "is" must provide a corresponding construction factory.
 
 pub trait Sym<R, D>
 where
-    R: Semiring
+    R: Semiring,
 {
     fn is(&self, c: &D) -> R;
 }
 
 pub enum Glui<R, D>
 where
-    R: Semiring
+    R: Semiring,
 {
     Eps,
     Sym(Rc<Sym<R, D>>),
@@ -49,40 +55,48 @@ where
 }
 
 // Empty, Final, Data
-pub struct Glu<R, D> (R, R, Glui<R, D>)
+pub struct Glu<R, D>(R, R, Glui<R, D>)
 where
     R: Semiring;
 
 /// Recognize only the empty string
 pub fn eps<R, D>() -> Rc<Glu<R, D>>
 where
-    R: Semiring
+    R: Semiring,
 {
-    Rc::new(Glu(R::one(), R::one(), Glui::Eps))
+    Rc::new(Glu(R::one(), R::zero(), Glui::Eps))
 }
 
 /// Recognize alternatives between two other regexes
-pub fn alt<R, D>(r1: &Rc<Glu<R, D>>, r2: &Rc<Glu<R, D>>) -> Rc<Glu<R, D>>
+pub fn alt<R, D>(l: &Rc<Glu<R, D>>, r: &Rc<Glu<R, D>>) -> Rc<Glu<R, D>>
 where
-    R: Semiring
+    R: Semiring,
 {
-    Rc::new(Glu(r1.0.add(&r2.0), r1.1.add(&r2.1), Glui::Alt(r1.clone(), r2.clone())))
+    Rc::new(Glu(
+        l.0.add(&r.0),
+        l.1.add(&r.1),
+        Glui::Alt(l.clone(), r.clone()),
+    ))
 }
 
 /// Recognize a sequence of regexes in order
-pub fn seq<R, D>(r1: &Rc<Glu<R, D>>, r2: &Rc<Glu<R, D>>) -> Rc<Glu<R, D>>
+pub fn seq<R, D>(l: &Rc<Glu<R, D>>, r: &Rc<Glu<R, D>>) -> Rc<Glu<R, D>>
 where
-    R: Semiring
+    R: Semiring,
 {
-    Rc::new(Glu(r1.0.add(&r2.0), r1.1.mul(&r2.0).add(&r2.1), Glui::Seq(r1.clone(), r2.clone())))
+    Rc::new(Glu(
+        l.0.mul(&r.0),
+        l.1.mul(&r.0).add(&r.1),
+        Glui::Seq(l.clone(), r.clone()),
+    ))
 }
 
 /// Recognize a regex repeated zero or more times.
-pub fn rep<R, D>(r1: &Rc<Glu<R, D>>) -> Rc<Glu<R, D>>
+pub fn rep<R, D>(c: &Rc<Glu<R, D>>) -> Rc<Glu<R, D>>
 where
-    R: Semiring + Clone
+    R: Semiring + Clone,
 {
-    Rc::new(Glu(R::one(), r1.1.clone(), Glui::Rep(r1.clone())))
+    Rc::new(Glu(R::one(), c.1.clone(), Glui::Rep(c.clone())))
 }
 
 // The main function: repeatedly traverses the tree, modifying as it
@@ -98,29 +112,28 @@ where
 {
     use self::Glui::*;
     match &g.2 {
-        Eps         => eps(),
-        Sym(f)      => Rc::new(Glu(R::zero(), m.mul(&f.is(c)), Glui::Sym(f.clone()))),
-        Alt(r1, r2) => alt(&shift(&r1, m, c), &shift(&r2, m, c)),
-        Seq(r1, r2) => seq(&shift(&r1, m, c), &shift(&r2, &(m.mul(&r1.0).add(&r1.1)), c)),
-        Rep(r)      => rep(&shift(&r, &(m.add(&r.1)), c)),
+        Eps => eps(),
+        Sym(f) => Rc::new(Glu(R::zero(), m.mul(&f.is(c)), Glui::Sym(f.clone()))),
+        Alt(l, r) => alt(&shift(&l, m, c), &shift(&r, m, c)),
+        Seq(l, r) => seq(&shift(&l, m, c), &shift(&r, &(m.mul(&l.0).add(&l.1)), c)),
+        Rep(r) => rep(&shift(&r, &(m.add(&r.1)), c)),
     }
 }
-    
+
 pub fn accept<R, D, I>(g: &Rc<Glu<R, D>>, source: &mut I) -> R
 where
-    R: Semiring + Clone,
-    I: Iterator<Item = D>
+    R: Semiring + Clone + PartialEq,
+    I: Iterator<Item = D>,
 {
-    let ashift = |g, c| { shift(&g, &R::zero(), &c) };
+    let ashift = |g, c| shift(&g, &R::zero(), &c);
 
     let start = if let Some(c) = source.next() {
         shift(g, &R::one(), &c)
     } else {
-        return g.0.clone()
+        return g.0.clone();
     };
     (&source.fold(start, ashift)).1.clone()
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -129,42 +142,64 @@ mod tests {
 
     macro_rules! set {
         ( $( $x:expr ),* ) => {{
+                #[allow(unused_mut)]
                 let mut temp_set = HashSet::new();
                 $( temp_set.insert($x); )*
                 temp_set //
             }};
     }
 
-    #[derive(Debug, Copy, Clone)]
+    //  ___                       _
+    // | _ \___ __ ___  __ _ _ _ (_)______
+    // |   / -_) _/ _ \/ _` | ' \| |_ / -_)
+    // |_|_\___\__\___/\__, |_||_|_/__\___|
+    //                 |___/
+
+    #[derive(Debug, Copy, Clone, PartialEq)]
     pub struct Recognizer(bool);
 
     impl Semiring for Recognizer {
-        fn one() -> Recognizer { Recognizer(true) }
-        fn zero() -> Recognizer { Recognizer(false) }
-        fn is_zero(&self) -> bool { !self.0 }
-        fn mul(&self, rhs: &Recognizer) -> Recognizer { Recognizer(self.0 && rhs.0) }
-        fn add(&self, rhs: &Recognizer) -> Recognizer { Recognizer(self.0 || rhs.0) }
+        fn one() -> Recognizer {
+            Recognizer(true)
+        }
+        fn zero() -> Recognizer {
+            Recognizer(false)
+        }
+        fn is_zero(&self) -> bool {
+            !self.0
+        }
+        fn mul(&self, rhs: &Recognizer) -> Recognizer {
+            Recognizer(self.0 && rhs.0)
+        }
+        fn add(&self, rhs: &Recognizer) -> Recognizer {
+            Recognizer(self.0 || rhs.0)
+        }
     }
 
-    pub struct SimpleSym
-    {
+    pub struct SimpleSym {
         c: char,
     }
-    
+
     impl Sym<Recognizer, char> for SimpleSym {
         fn is(&self, c: &char) -> Recognizer {
-            if *c == self.c { Recognizer::one() } else { Recognizer::zero() }
+            if *c == self.c {
+                Recognizer::one()
+            } else {
+                Recognizer::zero()
+            }
         }
     }
-            
+
     #[test]
     fn basics() {
-        
-        pub fn sym(sample: char) -> Rc<Glu<Recognizer, char>>
-        {
-            Rc::new(Glu(Recognizer::zero(), Recognizer::zero(), Glui::Sym(Rc::new(SimpleSym{ c: sample }))))
+        pub fn sym(sample: char) -> Rc<Glu<Recognizer, char>> {
+            Rc::new(Glu(
+                Recognizer::zero(),
+                Recognizer::zero(),
+                Glui::Sym(Rc::new(SimpleSym { c: sample })),
+            ))
         }
-        
+
         let cases = [
             ("empty", eps(), "", true),
             ("char", sym('a'), "a", true),
@@ -196,13 +231,75 @@ mod tests {
         }
     }
 
-    #[derive(Debug, Clone)]
+    //  _         __ _   _                __   __    _ _    _
+    // | |   ___ / _| |_| |___ _ _  __ _  \ \ / /_ _| (_)__| |
+    // | |__/ -_)  _|  _| / _ \ ' \/ _` |  \ V / _` | | / _` |
+    // |____\___|_|  \__|_\___/_||_\__, |   \_/\__,_|_|_\__,_|
+    //                             |___/
+
+    #[test]
+    fn assert_leftlong_tests_valid() {
+        pub struct AnySym {};
+        impl Sym<Recognizer, char> for AnySym {
+            fn is(&self, _: &char) -> Recognizer {
+                return { Recognizer::one() };
+            }
+        }
+
+        pub fn sym(sample: char) -> Rc<Glu<Recognizer, char>> {
+            Rc::new(Glu(
+                Recognizer::zero(),
+                Recognizer::zero(),
+                Glui::Sym(Rc::new(SimpleSym { c: sample })),
+            ))
+        }
+
+        pub fn asy() -> Rc<Glu<Recognizer, char>> {
+            Rc::new(Glu(
+                Recognizer::zero(),
+                Recognizer::zero(),
+                Glui::Sym(Rc::new(AnySym {})),
+            ))
+        }
+
+        let any = rep(&asy());
+        let a = sym('a');
+        let ab = rep(&alt(&a, &sym('b')));
+        let aaba = seq(&seq(&a, &ab), &a);
+
+        let cases = [
+            ("any", &seq(&any, &sym('c')), "cbcdc", true),
+            ("leftlong sample zero", &aaba, "ab", false),
+            ("leftlong sample five", &aaba, "bababa", true),
+            ("leftlong sample one", &aaba, "aa", true),
+        ];
+
+        for (name, case, sample, result) in &cases {
+            println!("{:?}", name);
+            let arb = seq(&any, &seq(&case, &any));
+            assert_eq!(accept(&arb, &mut sample.to_string().chars()).0, *result);
+        }
+    }
+
+    //  ___
+    // | _ \__ _ _ _ ___ ___
+    // |  _/ _` | '_(_-</ -_)
+    // |_| \__,_|_| /__/\___|
+    //
+
+    #[derive(Debug, Clone, PartialEq)]
     pub struct Parser(HashSet<String>);
 
     impl Semiring for Parser {
-        fn one() -> Parser { Parser(set!["".to_string()]) }
-        fn zero() -> Parser { Parser(set![]) }
-        fn is_zero(&self) -> bool { self.0.len() == 0 }
+        fn one() -> Parser {
+            Parser(set!["".to_string()])
+        }
+        fn zero() -> Parser {
+            Parser(set![])
+        }
+        fn is_zero(&self) -> bool {
+            self.0.len() == 0
+        }
         fn mul(self: &Parser, rhs: &Parser) -> Parser {
             let mut temp = set![];
             for i in self.0.iter().cloned() {
@@ -218,22 +315,29 @@ mod tests {
     }
 
     pub struct ParserSym {
-        c: char
+        c: char,
     }
 
     impl Sym<Parser, char> for ParserSym {
         fn is(&self, c: &char) -> Parser {
-            if *c == self.c { Parser(set![c.to_string()]) } else { Parser::zero() }
+            if *c == self.c {
+                Parser(set![c.to_string()])
+            } else {
+                Parser::zero()
+            }
         }
     }
 
     #[test]
     fn string_basics() {
-        pub fn sym(sample: char) -> Rc<Glu<Parser, char>>
-        {
-            Rc::new(Glu(Parser::zero(), Parser::zero(), Glui::Sym(Rc::new(ParserSym{ c: sample }))))
+        pub fn sym(sample: char) -> Rc<Glu<Parser, char>> {
+            Rc::new(Glu(
+                Parser::zero(),
+                Parser::zero(),
+                Glui::Sym(Rc::new(ParserSym { c: sample })),
+            ))
         }
-        
+
         let cases = [
             ("empty", eps(), "", Some("")),
             ("char", sym('a'), "a", Some("a")),
@@ -250,8 +354,18 @@ mod tests {
             ("bad short sequence", seq(&sym('a'), &sym('b')), "a", None),
             ("one rep", rep(&sym('a')), "a", Some("a")),
             ("short multiple failed rep", rep(&sym('a')), "ab", None),
-            ("multiple rep", rep(&sym('a')), "aaaaaaaaa", Some("aaaaaaaaa")),
-            ("multiple rep with failure", rep(&sym('a')), "aaaaaaaaab", None),
+            (
+                "multiple rep",
+                rep(&sym('a')),
+                "aaaaaaaaa",
+                Some("aaaaaaaaa"),
+            ),
+            (
+                "multiple rep with failure",
+                rep(&sym('a')),
+                "aaaaaaaaab",
+                None,
+            ),
         ];
 
         for (name, case, sample, result) in &cases {
@@ -266,125 +380,143 @@ mod tests {
                         panic!("Strings did not match: {:?}, {:?}", r, v);
                     }
                     assert_eq!(1, ret.len());
-                },
-                None    => assert_eq!(0, ret.len())
+                }
+                None => assert_eq!(0, ret.len()),
             }
         }
     }
 
+    //  _         __ _   _
+    // | |   ___ / _| |_| |___ _ _  __ _
+    // | |__/ -_)  _|  _| / _ \ ' \/ _` |
+    // |____\___|_|  \__|_\___/_||_\__, |
+    //                             |___/
+
     // The semiring and semiring_i implementations for the LeftLong
     // Range operations.
-    
+
     pub trait Semiringi<S: Semiring> {
         fn index(i: usize) -> S;
     }
 
     #[derive(Debug, Clone, Eq, PartialEq)]
-    pub enum RRanges {
-        Notfound,
-        Scanning,
-        Found(usize, usize)
+    pub enum Leftlong {
+        Notfound,            // Zero
+        Scanning,            // One
+        Found(usize, usize), // The set of useful datapoints
     }
-    
-    #[derive(Debug, Clone, Eq, PartialEq)]
-    pub struct Leftlong(RRanges);
 
     impl Semiring for Leftlong {
-        fn one() -> Leftlong { Leftlong(RRanges::Scanning) }
-        fn zero() -> Leftlong { Leftlong(RRanges::Notfound) }
-        fn is_zero(&self) -> bool { matches!(self.0, RRanges::Notfound) }
+        fn one() -> Leftlong {
+            Leftlong::Scanning
+        }
+        fn zero() -> Leftlong {
+            Leftlong::Notfound
+        }
+        fn is_zero(&self) -> bool {
+            matches!(self, Leftlong::Notfound)
+        }
         fn add(self: &Leftlong, rhs: &Leftlong) -> Leftlong {
-            match self.0 {
-                RRanges::Notfound | RRanges::Scanning => rhs.clone(),
-                RRanges::Found(i, j) => {
-                    match rhs.0 {
-                        RRanges::Notfound | RRanges::Scanning => self.clone(),
-                        RRanges::Found(k, l) => {
-                            if i < k || (i == k && j > l) {
-                                self.clone()
-                            } else {
-                                rhs.clone()
-                            }
-                        }
+            use self::Leftlong::*;
+            match (self, rhs) {
+                (Notfound, c) | (c, Notfound) | (Scanning, c) | (c, Scanning) => c.clone(),
+                (Found(i, j), Found(k, l)) => {
+                    if i < k || i == k && j >= l {
+                        self.clone()
+                    } else {
+                        rhs.clone()
                     }
                 }
             }
         }
 
         fn mul(self: &Leftlong, rhs: &Leftlong) -> Leftlong {
-            match self.0 {
-                RRanges::Notfound => Leftlong(RRanges::Notfound),
-                RRanges::Scanning  => rhs.clone(),
-                RRanges::Found(i, _) => {
-                    match rhs.0 {
-                        RRanges::Notfound => Leftlong(RRanges::Notfound),
-                        RRanges::Scanning  => self.clone(),
-                        RRanges::Found(_, l) => Leftlong(RRanges::Found(i, l))
-                    }
-                }
+            use self::Leftlong::*;
+            match (self, rhs) {
+                (Notfound, _) | (_, Notfound) => Leftlong::zero(),
+                (Scanning, c) | (c, Scanning) => c.clone(),
+                (Found(i, _), Found(_, l)) => Leftlong::Found(*i, *l),
             }
         }
     }
 
     impl Semiringi<Leftlong> for Leftlong {
         fn index(i: usize) -> Leftlong {
-            Leftlong(RRanges::Found(i, i))
+            Leftlong::Found(i, i)
         }
     }
 
     // The position and character being analyzed.
-    
+
     pub struct Pc(usize, char);
 
-    #[derive(Debug, Clone)]
+    #[derive(Clone)]
     pub struct RangeSym(char);
 
     impl Sym<Leftlong, Pc> for RangeSym {
         fn is(&self, pc: &Pc) -> Leftlong {
-            if pc.1 == self.0 { Leftlong::index(pc.0) } else { Leftlong(RRanges::Notfound) }
+            if pc.1 == self.0 {
+                Leftlong::index(pc.0)
+            } else {
+                Leftlong::zero()
+            }
         }
     }
 
     #[derive(Debug, Clone)]
-    pub struct AnySym{}
-    impl Sym<Leftlong, Pc> for AnySym
-    {
-        fn is(&self, _: &Pc) -> Leftlong { return Leftlong::one() }
-    }
-    
-    pub fn asym() -> Rc<Glu<Leftlong, Pc>>
-    {
-        Rc::new(Glu(Leftlong::zero(), Leftlong::zero(), Glui::Sym(Rc::new(AnySym{}))))
+    pub struct AnySym {}
+    impl Sym<Leftlong, Pc> for AnySym {
+        fn is(&self, _: &Pc) -> Leftlong {
+            Leftlong::one()
+        }
     }
 
-    
-
-    pub fn submatch(g: &Rc<Glu<Leftlong, Pc>>, s: &str) -> Leftlong
-    {
-        let arb = seq(&rep(&asym()), &seq(g, &rep(&asym())));
-        accept(&arb, &mut s.to_string().chars().into_iter().enumerate().map(|c| Pc(c.0, c.1)))
+    pub fn asym() -> Rc<Glu<Leftlong, Pc>> {
+        Rc::new(Glu(
+            Leftlong::zero(),
+            Leftlong::zero(),
+            Glui::Sym(Rc::new(AnySym {})),
+        ))
     }
 
     #[test]
     fn leftlong_basics() {
-        pub fn symi(sample: char) -> Rc<Glu<Leftlong, Pc>>
-        {
-            Rc::new(Glu(Leftlong::zero(), Leftlong::zero(), Glui::Sym(Rc::new(RangeSym(sample)))))
+        pub fn symi(sample: char) -> Rc<Glu<Leftlong, Pc>> {
+            Rc::new(Glu(
+                Leftlong::zero(),
+                Leftlong::zero(),
+                Glui::Sym(Rc::new(RangeSym(sample))),
+            ))
         }
 
         let a = symi('a');
-        let ab = rep(&alt(&a, &symi('b')));
-        let aaba = seq(&a, &seq(&ab, &a));
-        
+        let ab = rep(&alt(&a.clone(), &symi('b')));
+        let aaba = seq(&a.clone(), &seq(&ab, &a.clone()));
+
         let cases = [
-            ("leftlong zero", &aaba, "ab",     Leftlong(RRanges::Notfound)),
-            ("leftlong one",  &aaba, "aa",     Leftlong(RRanges::Found(0, 1))),
-            ("leftlong five", &aaba, "bababa", Leftlong(RRanges::Found(1, 5)))
+            ("leftlong zero", &aaba.clone(), "ab", Leftlong::Notfound),
+            ("leftlong one", &aaba.clone(), "aa", Leftlong::Found(0, 1)),
+            (
+                "leftlong five",
+                &aaba.clone(),
+                "bababa",
+                Leftlong::Found(1, 5),
+            ),
         ];
 
         for (name, case, sample, result) in &cases {
             println!("{:?}", name);
-            let ret = submatch(case, &sample.to_string());
+            let any = rep(&asym());
+            let arb = seq(&any, &seq(&case, &any));
+            let ret = accept(
+                &arb,
+                &mut sample
+                    .to_string()
+                    .chars()
+                    .into_iter()
+                    .enumerate()
+                    .map(|c| Pc(c.0, c.1)),
+            );
             assert_eq!(result, &ret);
         }
     }
